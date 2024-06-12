1ef3b5123731cfd2f7218964b6afc5f982c93788
John <jonja-148@hotmail.com>
2024-06-12 15:08:46 -0500

    Fix error de Auth ruta

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+import styles from "./title.module.css";
+
+interface TitleProps {
+  title: string;
+  color: "violeta" | "beige";
+}
+export const Title: FC<TitleProps> = ({ title, color }) => {
+  let bgColor =
+    color == "violeta"
+      ? "after:bg-violeta before:bg-violeta"
+      : "after:bg-beige before:bg-beige";
+  return (
+      <h2 className={`${styles.typography_title} text-${color} ${bgColor}`}>
+        {title}
+      </h2>
+  );
+};
